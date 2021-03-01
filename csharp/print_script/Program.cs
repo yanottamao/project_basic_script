b8e25c7d6e82b70d7e8ba996460867de412273f1
yanottamao <oktatama4@gmail.com>
2021-03-01 13:14:07 +0700

    Add print example in c# dotnet

---
+++ csharp/print_script/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace print_script
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
