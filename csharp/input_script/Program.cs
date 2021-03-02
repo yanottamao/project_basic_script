b542b131b34882273f5694b4f2ef0b6879b3b9e2
yanottamao <oktatama4@gmail.com>
2021-03-02 18:30:12 +0700

    Add user input example in c# dotnet

---
+++ csharp/input_script/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace input_script
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username: ");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Username is: " + userName);
+        }
+    }
+}
